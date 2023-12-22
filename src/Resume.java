daa5ee13e430364bf40412b2cf71bb3c57b271b3
Jacquelyn <jacquelyns131@gmail.com>
2023-12-21 21:09:28 -0500

    work on Resume, add comment to Contact class

--- src/Resume.java
+++ src/Resume.java
@@ -3,6 +3,23 @@ import java.util.ArrayList;
 public class Resume {
     private Contact contacts;
     private ArrayList<Club> clubs;
+    private ArrayList<Education> education;
+    private ArrayList<Project> projects;
+    private Skills skills;
+    private ArrayList<WorkExp> workExps;
+
+    // constructor
+    public Resume() {
+        this.contacts = null;
+        this.clubs = new ArrayList<Club>();
+        this.education = new ArrayList<Education>();
+        this.projects = new ArrayList<Project>();
+        this.workExps = new ArrayList<WorkExp>();
+        this.skills = new Skills();
+    }
+    // getters
+
+    // setters
     // TODO: finish fields and constructor and methods
 
 }
