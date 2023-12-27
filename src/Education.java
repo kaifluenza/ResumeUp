818ce7c478d3f4e1c9c41a8dbe1aba7a43d93bd9
kai34000 <wanrueni.12@gmail.com>
2023-12-26 19:42:24 -0500

    Modify Education class

--- src/Education.java
+++ src/Education.java
@@ -1,2 +1,62 @@
 public class Education {
+    private String school;
+    private String startYear;
+    private String gradYear;
+    private String major;
+    private String minor;
+
+    public Education(String school, String startYear, String gradYear, String major, String minor) {
+        this.school = school;
+        this.startYear = startYear;
+        this.gradYear = gradYear;
+        this.major = major;
+        this.minor = minor;
+    }
+
+    public Education(String school, String startYear, String gradYear, String major) {
+        this.school = school;
+        this.startYear = startYear;
+        this.gradYear = gradYear;
+        this.major = major;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public String getStartYear() {
+        return startYear;
+    }
+
+    public void setStartYear(String startYear) {
+        this.startYear = startYear;
+    }
+
+    public String getGradYear() {
+        return gradYear;
+    }
+
+    public void setGradYear(String gradYear) {
+        this.gradYear = gradYear;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public String getMinor() {
+        return minor;
+    }
+
+    public void setMinor(String minor) {
+        this.minor = minor;
+    }
 }
