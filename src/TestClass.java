8ea5f2ba902ddf3940aab7308d8fad9677c7d98c
Jacquelyn <jacquelyns131@gmail.com>
2023-12-27 22:17:06 -0500

    fix bugs in displayresume

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,4 +1,7 @@
 public class TestClass {
+    public TestClass() {
+    }
+
     public static void testDisplayResume() {
         Resume res = new Resume();
         res.setContacts(new Contact("Stephanie", "111-111-1111", "Brooklyn, NY"));
@@ -7,8 +10,11 @@ public class TestClass {
                 "Minor in Mathematics", "3.63/4.0"));
         res.addExperience(new WorkExp("Nurse", "jul 2003",  "Sep 2010", "Boston, MA",
                 "Big Hospital"));
+        res.getWorkExps().get(0).addBulletPoint("Performed CPR and saved the lives of a million people");
         res.addSkill("Java");
         res.addProject(new Project("Stock trading mobile app", "Aug 2004", "Boston, MA"));
+        res.getProjects().get(0).addBulletPoint("Used Android Studio and Java to develop the top-ranked app in stocks" +
+                " and finance 2 years in a row.");
         res.displayResume();
     }
 
